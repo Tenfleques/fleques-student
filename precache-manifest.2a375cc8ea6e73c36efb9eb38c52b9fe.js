93334290b3f78a15d177419110b9f3717c202246
Tendai Chikake <tendaichikae@gmail.com>
2020-01-25 01:39:19 +0300

    Updates

---
+++ precache-manifest.2a375cc8ea6e73c36efb9eb38c52b9fe.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eccbf5c8222d4ccd815f865a72ce8200",
+    "url": "/fleques-student/index.html"
+  },
+  {
+    "revision": "6328cb6f8b84284f8e46",
+    "url": "/fleques-student/static/css/main.17b74ad8.chunk.css"
+  },
+  {
+    "revision": "fbaa704ef9ae53271963",
+    "url": "/fleques-student/static/js/2.b986d49c.chunk.js"
+  },
+  {
+    "revision": "6328cb6f8b84284f8e46",
+    "url": "/fleques-student/static/js/main.392ffed0.chunk.js"
+  },
+  {
+    "revision": "8bf1ceb725f4db89acd6",
+    "url": "/fleques-student/static/js/runtime-main.334ed17d.js"
+  }
+]);
